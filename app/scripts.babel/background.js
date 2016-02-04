50bc3cf86e911af6352dfe5ce6a042c25ca28dfb
Everlag <kyleveb@gmail.com>
2016-02-03 23:43:18 -0700

    clean up badge ui

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -10,6 +10,9 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
   console.log('got a message!', msg);
 
   if (msg.subject === 'badge') {
+    // Don't bother setting useless text.
+    if (msg.count === 0) return;
+
     chrome.browserAction.setBadgeText({
       text: msg.count.toString(),
       tabId: sender.tab.id
